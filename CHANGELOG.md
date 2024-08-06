b48f10481eca644c7a7b48946cc5db0ac731808e
lukasNo1 <lukas.wegmueller@goldinteractive.ch>
2024-08-06 10:26:55 +0200

    Fix styling and toggle all in element slideout container

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 2.1.1 - 2024-08-06
+
+### Fixed
+
+-  Fixed the toggle all checkbox and the styling in the element slideout container
+
 ## 2.1.0 - 2024-06-10
 
 ### Added
--- src/assetbundles/sitecopy/dist/js/sitecopy.js
+++ src/assetbundles/sitecopy/dist/js/sitecopy.js
@@ -1,5 +1,6 @@
 function toggleSitecopyTargets(source) {
-  const checkboxes = document.getElementsByName('sitecopy[targets][]');
+  const sitecopyDiv = source.closest('.sitecopy');
+  const checkboxes = sitecopyDiv.querySelectorAll('.sitecopy-options input[type="checkbox"]');
   const isChecked = source.checked;
 
   for (let i = 1, n = checkboxes.length; i < n; i++) {
@@ -7,11 +8,13 @@ function toggleSitecopyTargets(source) {
   }
 }
 
-function updateSitecopyToggleAll() {
-  const toggleAll = document.getElementById('sitecopy-toggle-all');
+function updateSitecopyToggleAll(source) {
+  const sitecopyDiv = source.closest('.sitecopy');
+  const toggleAll = sitecopyDiv.querySelector('[id$="sitecopy-toggle-all"]');
+
 
   if (toggleAll) {
-    const checkboxes = document.getElementsByName('sitecopy[targets][]');
+    const checkboxes = sitecopyDiv.querySelectorAll('.sitecopy-options input[type="checkbox"]');
     toggleAll.checked = Array.from(checkboxes).every((checkbox) => checkbox.checked);
   }
 }
